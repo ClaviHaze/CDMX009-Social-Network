741c9e6aee238ed9af781b3f8f766492b884a625
TangCherry <danaee16@gmail.com>
2020-08-18 20:50:23 -0500

    Updates

---
+++ precache-manifest.f49d187cabc232b7b212dc6e6a6fc2df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbbaae7fa5e10748e754cd05b7e5e1c4",
+    "url": "/CDMX009-Social-Network/index.html"
+  },
+  {
+    "revision": "55b4e0d191a336fee202",
+    "url": "/CDMX009-Social-Network/static/css/main.1c8980c5.chunk.css"
+  },
+  {
+    "revision": "7711d931ca2c51549a01",
+    "url": "/CDMX009-Social-Network/static/js/2.4ab310dd.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/CDMX009-Social-Network/static/js/2.4ab310dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55b4e0d191a336fee202",
+    "url": "/CDMX009-Social-Network/static/js/main.4186ddad.chunk.js"
+  },
+  {
+    "revision": "35f70624f8543b45d46f",
+    "url": "/CDMX009-Social-Network/static/js/runtime-main.6c4b9405.js"
+  }
+]);
